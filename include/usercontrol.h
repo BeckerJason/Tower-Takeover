03f637494e308ea7b6361fd5d4a4b9efeb868df4
Jason <jman5438212@gmail.com>
2020-03-30 22:50:22 -0400

    V1

---
+++ include/usercontrol.h
@@ -0,0 +1,93 @@
+#include "C:/Program Files (x86)/VEX Robotics/VEXcode/sdk/vexv5/include/v5_apitypes.h"
+#include "C:/Program Files (x86)/VEX Robotics/VEXcode/sdk/vexv5/include/vex_global.h"
+#include "defines.h"
+using namespace G;
+using namespace std;
+using namespace vex;
+
+#ifdef DEBUG // if DEBUG is not defined earlier this section will not compile
+/*while(DEBUG){
+    if (bUp == 1) { GLOBALP += .01; wait(100); }
+            if (bDown == 1) { GLOBALP -= .01; wait(100); }
+    if (bX== 1) { GLOBALI += .0000001; wait(100); }
+            if (bB == 1) {GLOBALI -= .0000001; wait(100); }
+    if (bR1== 1) { GLOBALD += .05; wait(100); }
+            if (bR2 == 1) {GLOBALD -= .05; wait(100); }
+            wait(10);
+}  */
+#endif 
+AutoRunning = 0;
+// SnapToCube = off;
+GlobalFlagOffset = 160;
+MATCHTIMER = 0;
+while (1) {
+  if (AutoRunning == 0 && (abs(ch3) > 10 || abs(ch1) > 10)) {
+    
+      run(LF, (ch3 + ch1/2)); //(Axis3+Axis4)/2
+      run(LM, (ch3 + ch1/2));
+      run(LB, (ch3 + ch1/2)); //(Axis3+Axis4)/2
+      run(RF, (ch3 - ch1/2)); //(Axis3-Axis4)/2
+      run(RM, (ch3 - ch1/2));
+      run(RB, (ch3 - ch1/2)); //(Axis3-Axis4)/2
+
+       
+    
+  } else if (AutoRunning == 0) {
+    StopDrive(brake);
+  } else {
+  }
+//ARM
+  if (AutoRunning == 0 && (abs(ch2) > 5))//Chain bar arm control
+    {
+      run(ArmL, ch2/5);
+      run(ArmR, ch2/5);
+      //ArmL.resetRotation();
+    }
+  else
+    {
+        
+      if(abs(enc(ArmL)) <10)
+      {
+        run(ArmL, 0);
+        run(ArmR, 0);
+      }
+      else
+      {
+        BRAKE(ArmL, hold);
+        BRAKE(ArmR, hold);
+      }
+    }
+/*//CASCADE LIFT
+if ((bR1 || bR2) == 1)
+{
+  CascadeControl();
+}
+else
+{
+  StopLift(brake);
+}*/
+
+
+
+
+  if (ramp == bwrd) {if (bA && !G::rampprev) {ramp = fwrd;RunRamp=on;}} 
+  else if (ramp == fwrd) {if ( bA && !G::rampprev) {ramp = bwrd;RunRamp=on;}}
+    G::rampprev=bA ;
+    if (bX==1){RunRamp=off;}
+
+  if(RampLimit.pressing()==0||RunRamp==on){}
+  else if (bY==1){run(RampWheelL,-100);run(RampWheelR,-100);}
+  else if ( (CubeSense.value(percentUnits::pct)<68 || CubeSense2.value(percentUnits::pct)<68) &&  bL1==0){run(RampWheelL,100);run(RampWheelR,100);}
+  else {BRAKE(RampWheelL,hold);BRAKE(RampWheelR,hold);}
+  
+ if(RampLimit.pressing()==0||RunRamp==on){} 
+  else if (intake == off) {if (bB && !G::intakeprev) intake = on;run(RightRoller, 80); run(LeftRoller, 80);} 
+  else if (intake == on) { 
+
+    if ( bB && !G::intakeprev) intake = off;run(RightRoller, 0); run(LeftRoller, 0);}
+       G::intakeprev = bB;
+       //INTAKE
+if (bY==1){run(RightRoller, -80); run(LeftRoller, -80);}
+  wait(20);
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       Jason                                                     */
+/*    Created:      Monday July 29 2019                                        */
+/*    Description:  V5 project                                                */
+/*                                                                             */
+/*----------------------------------------------------------------------------*/
+
+//red 2 blue 3
+#include "defines.h"
+vex::competition Competition;  
+#include "functions.h" 
+
+//
+void pre_auton(void) {
+  preautoL=true;
+#include "pre_auton.h" 
+}
+
+//
+void auton(void) { 
+  preautoL=false;
+  G::MATCHTIMER=0;
+  vex::task starttimer (ENDAUTOTIMER);
+  vex::task rampcontroller (RampControl);
+#include "autonincludes.h"
+}
+
+
+void usercontrol(void) {
+  preautoL=false;
+G::MATCHTIMER=0; 
+vex::task starttimer (ENDAUTOTIMER);
+vex::task rampcontroller (RampControl);
+ #include "usercontrol.h"
+} 
+
+int main() {
+
+AutoRunning = 0;
+RightRoller.resetRotation();
+LeftRoller.resetRotation();
+ArmL.resetRotation();
+   wait(1000);
+   Gyro.startCalibration();
+   wait(3000);
+	//Run the pre-autonomous function. 
+	pre_auton();
+
+	//Set up callbacks for autonomous and driver control periods.
+	Competition.autonomous(auton);
+	Competition.drivercontrol(usercontrol);
+
+	//Prevent main from exiting with an infinite loop.                        
+	while (1) {
+		wait(100);//Sleep the task for a short amount of time to prevent wasted resources.
+	}
+  return 0;
+}
+
+
